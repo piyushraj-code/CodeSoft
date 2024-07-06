bb0d08b79b04af4f1debc1f66757a46ffab83ca7
Piyush Raj <raj4piyush@gmail.com>
2024-07-06 22:17:26 +0530

    CodeSoft Internship

---
+++ StudentGradeCalculator.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class StudentGradeCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of subjects: ");
+        int numSubjects = scanner.nextInt();
+
+        double totalMarks = 0;
+        for (int i = 0; i < numSubjects; i++) {
+            System.out.print("Enter marks obtained in subject " + (i + 1) + " (out of 100): ");
+            double marks = scanner.nextDouble();
+            totalMarks += marks;
+        }
+
+        double averagePercentage = totalMarks / numSubjects;
+
+        char grade;
+        if (averagePercentage >= 90) {
+            grade = 'A';
+        } else if (averagePercentage >= 80) {
+            grade = 'B';
+        } else if (averagePercentage >= 70) {
+            grade = 'C';
+        } else if (averagePercentage >= 60) {
+            grade = 'D';
+        } else {
+            grade = 'F';
+        }
+
+        System.out.printf("Total Marks: %.2f%n", totalMarks);
+        System.out.printf("Average Percentage: %.2f%%%n", averagePercentage);
+        System.out.println("Grade: " + grade);
+    }
+}
