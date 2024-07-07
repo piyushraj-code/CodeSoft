2c81917109b5fe1911409a8fdab57cb449f966a1
piyushraj-code <raj4piyush@gmail.com>
2024-07-07 21:07:15 +0530

    Update StudentGradeCalculator.java

--- StudentGradeCalculator.java
+++ StudentGradeCalculator.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Grade {
+public class StudentGradeCalculator {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
