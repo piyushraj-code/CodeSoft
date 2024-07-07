f28aa128442f4466ff2f8688b71bde5f46052b0b
Piyush Raj <raj4piyush@gmail.com>
2024-07-07 11:42:50 +0530

    CodeSoft Internship

--- AtmInterface.java
+++ AtmInterface.java
@@ -92,7 +92,7 @@ class ATM {
     }
 }
 
-public class Main {
+public class AtmInterface {
     public static void main(String[] args) {
         BankAccount account = new BankAccount(1000.0); // Initial balance of $1000
         ATM atm = new ATM(account);
