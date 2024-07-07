8cb09bfe22f288a1df627230a5435bb147226631
piyushraj-code <raj4piyush@gmail.com>
2024-07-07 21:06:40 +0530

    Update StudentGradeCalculator.java

--- StudentGradeCalculator.java
+++ StudentGradeCalculator.java
@@ -1,36 +1,22 @@
 import java.util.Scanner;
 
-public class StudentGradeCalculator {
+public class Grade {
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter the number of subjects: ");
-        int numSubjects = scanner.nextInt();
-
-        double totalMarks = 0;
-        for (int i = 0; i < numSubjects; i++) {
-            System.out.print("Enter marks obtained in subject " + (i + 1) + " (out of 100): ");
-            double marks = scanner.nextDouble();
-            totalMarks += marks;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of subjects:");
+        int n = sc.nextInt();
+        int[] marks = new int[n];
+        for (int i = 1; i <= n; i++){
+            System.out.println("Enter marks of subject"+i);
+            marks[i-1] = sc.nextInt();
         }
-
-        double averagePercentage = totalMarks / numSubjects;
-
-        char grade;
-        if (averagePercentage >= 90) {
-            grade = 'A';
-        } else if (averagePercentage >= 80) {
-            grade = 'B';
-        } else if (averagePercentage >= 70) {
-            grade = 'C';
-        } else if (averagePercentage >= 60) {
-            grade = 'D';
-        } else {
-            grade = 'F';
+        int totalMarks = 0;
+        for (int i = 0; i < marks.length; i++) {
+            totalMarks += marks[i];
         }
-
-        System.out.printf("Total Marks: %.2f%n", totalMarks);
-        System.out.printf("Average Percentage: %.2f%%%n", averagePercentage);
-        System.out.println("Grade: " + grade);
+        double precntage = totalMarks/n;
+        System.out.println("Your total marks is: " + totalMarks);
+        System.out.println("Your socred: "+ precntage +"%");
     }
 }
